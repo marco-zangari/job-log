735dbf8093d7183bca760132eca8d600a340d192
Marco Zangari <mjzangari@gmail.com>
2018-03-12 14:30:30 -0700

    adding django framework

--- job_logs/models.py
+++ job_logs/models.py
@@ -7,11 +7,26 @@ from django.db import models
 class Topic(models.Model):
     """Topic the user learns about."""
 
-    company = models.CharField(max_length=200)
     text = models.CharField(max_length=200)
-    contact = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """Return string of the model."""
         return self.text
+
+
+class Entry(models.Model):
+    """Something specific learned about the company."""
+
+    topic = models.ForeignKey(Topic)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """."""
+
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return string representation of model."""
+        return self.text[:50] + "..."
