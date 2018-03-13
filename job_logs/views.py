8ae40253c8f94068cfebb720bbcf565487fb36cc
Marco Zangari <mjzangari@gmail.com>
2018-03-12 17:02:50 -0700

    added new html stuff

--- job_logs/views.py
+++ job_logs/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
+from .models import Topic
 # Create your views here.
 
 
 def index(request):
     """Home page for JobLog."""
     return render(request, 'job_logs/index.html')
+
+
+def topics(request):
+    """Show all the topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'job_logs/topics.html', context)
