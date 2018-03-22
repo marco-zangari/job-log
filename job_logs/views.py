2a058d0955faea3add0d22b93d28f04c78cd6c57
Marco Zangari <mjzangari@gmail.com>
2018-03-21 17:25:59 -0700

    added fixes

--- job_logs/views.py
+++ job_logs/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 # Create your views here.
 
@@ -55,3 +55,20 @@ def new_entry(request, topic_id):
             return HttpResponseRedirect(reverse('job_logs:topic', args=[topic_id]))
     context = {'topic': topic, 'form': form}
     return render(request, 'job_logs/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('job_logs:topic', args=[topic.id]))
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'job_logs/edit_entry.html', context)
